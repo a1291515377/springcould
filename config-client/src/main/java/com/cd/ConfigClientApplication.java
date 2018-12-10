1bf4bfb52158439a4ad76bb8379d19ac83a8331d
a1291515377 <45416535+a1291515377@users.noreply.github.com>
2018-12-10 09:21:01 +0800

    config-client
    配置中心消费端

---
+++ config-client/src/main/java/com/cd/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package com.cd;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableDiscoveryClient
+@RefreshScope       //actuator给我们提供了一个/refresh接口
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
